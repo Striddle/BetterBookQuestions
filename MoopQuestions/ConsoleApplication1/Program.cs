2f76f92464dd18634a10fc70305244ffa052dff8
Striddle <Striddle@hotmail.com>
2013-10-23 07:58:56 -0400

    MOOPALEEDEELEE DOOP

---
+++ MoopQuestions/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double price;
+            string Area;
+            string Minutes;
+            int[] AreaCodes = new int[6]{262, 414, 608, 715, 815, 920};
+            double[] rates = new double[6]{.07, .10, .05, .16, .24, .14};
+                
+            Console.WriteLine ("WAT IS YO AREA CODE?");
+            Area = Console.ReadLine();
+            Console.WriteLine("WAT WAS YO LENGTH OF CALL?");
+            Minutes = Console.ReadLine();
+
+            for (int i = 0; i< AreaCodes.Length; i++)
+            {
+                if (Area == AreaCodes[i].ToString())
+                {
+                    price = rates[i] * Convert.ToInt32(Minutes);
+                    Console.WriteLine(price.ToString ("C"));
+                }
+            }
+        }
+    }
+}
--- MoopQuestions/MoopQuestions/Program.cs
+++ MoopQuestions/MoopQuestions/Program.cs
@@ -3,12 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace MoopQuestions
+namespace BookQuestions
 {
-    class Program
+    class Furniture
     {
         static void Main(string[] args)
         {
+            int Price = 0;
+            Console.WriteLine("(P)ine, (O)ak, or (M)ahogony");
+            string input = Console.ReadLine();
+            if (input == "P" || input == "p")
+            {
+                Price += 100;
+                Console.Write(Price.ToString("C"));
+            }
+            else if (input == "O" || input == "o")
+            {
+                Price += 225;
+                Console.Write(Price.ToString("C"));
+            }
+            else if (input == "M" || input == "m")
+            {
+                Price += 310;
+                Console.Write(Price.ToString("C"));
+            }
+            else
+            {
+                Price += 0;
+                Console.Write(Price.ToString("C"));
+            }
+
         }
     }
 }
