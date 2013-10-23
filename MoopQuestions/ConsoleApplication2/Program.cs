432e0cb7bddc51919cc70f5fbabe015510fda998
Striddle <Striddle@hotmail.com>
2013-10-23 08:25:29 -0400

    THAT'S A LOT OF MOOPLA

--- MoopQuestions/ConsoleApplication2/Program.cs
+++ MoopQuestions/ConsoleApplication2/Program.cs
@@ -9,6 +9,28 @@ namespace ConsoleApplication2
     {
         static void Main(string[] args)
         {
+            string CarPrice;
+            double value;
+            double Commission;
+
+            Console.WriteLine("What price was the car you sold?");
+            CarPrice = Console.ReadLine();
+            value = Convert.ToDouble(CarPrice);
+
+            if (value <= 15000)
+            {
+                Console.WriteLine(Commission = value * .05);
+            }
+            else if (value <= 24000 && value > 15000)
+            {
+                Console.WriteLine(Commission = value * .07);
+            }
+            else
+            {
+                Console.WriteLine(Commission = value * .1 );
+            }
+            Console.WriteLine("Your commission is: $" + Commission);
         }
+
     }
 }
