01ce90a58197b102ad3ec4871fca73288bf3508e
Striddle <Striddle@hotmail.com>
2013-10-24 08:03:38 -0400

    3 MOOPS LEFT

---
+++ MoopQuestions/Moopagrams/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Moopagrams
+{
+    class Monogram
+    {
+        static void Main()
+        {
+            DisplayMonogram("S", "T", "R");
+            DisplayMonogram("K", "W", "H");
+            Console.ReadKey();
+        }
+        public static void DisplayMonogram(string first, string middle, string last)
+        {
+            Console.WriteLine("** {0}. {1}. {2} **", first, middle, last);
+        }
+    }
+}
