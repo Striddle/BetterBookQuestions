62c4075d260ad4936ba934ba33c8af05ec586c40
Striddle <Striddle@hotmail.com>
2013-10-24 08:22:10 -0400

    MOOPALEEDEELE DEEEP OOWPOOWAT MOOPLADO

---
+++ MoopQuestions/MOOPonents/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MOOPonents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            Console.WriteLine("enter a noomber");
+            input = Console.ReadLine();
+
+            Console.WriteLine(Math.Pow(Convert.ToInt32(input), 2));
+            Console.WriteLine(Math.Pow(Convert.ToInt32(input), 3));
+        }
+    }
+}
